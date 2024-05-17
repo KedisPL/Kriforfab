2c8c234ff805ca0898ef99a5e37d85a065471596
TKD_Kedis <elektrogamepl@gmail.com>
2024-05-17 17:50:00 +0200

    0.4
    - Fixed incompatibility issues.
    - Divided into modules.

---
+++ fabric-api-base/src/main/java/net/fabricmc/fabric/api_base_init/ApiBaseForge.java
@@ -0,0 +1,7 @@
+package net.fabricmc.fabric.api_base_init;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("fabric_api_base")
+public class ApiBaseForge {
+}
---
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/biome_api_init/BiomeApiForge.java
@@ -0,0 +1,7 @@
+package net.fabricmc.fabric.biome_api_init;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("fabric_biome_api_v1")
+public class BiomeApiForge {
+}
